98cde36b5ff843bc5802d8868357d6a60131f2d7
Ema-sk8 <119638007+Ema-sk8@users.noreply.github.com>
2023-04-02 01:12:04 -0600

    praticaterminada

---
+++ js/validaciones.js
@@ -0,0 +1,103 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+     
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+ 
+const tipoDeErrores =[
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
+const mensajesDeError = {
+    nombre:{
+        valueMissing:"El campo nombre no puede estar vacio"
+    },
+    email:{
+        valueMissing:"El campo correo no puede estar vacio",
+        typeMismatch:"El correo no es valido"
+    },
+    password:{
+        valueMissing:"El campo contraseña no puede quedar vacio",
+        patternMismatch: "Al menos 6 caracteres, maximo 12, debeontener una letra minuscula, una letra mayuscula y no puede contener ccaracteres especiales"
+    },
+    nacimiento:{
+        valueMissing:"Este campo no puede estar vacio",
+        customError:"Debes tener al menos 18 años de edad"
+    },
+    numero:{
+        valueMissing:"El campo de numero no puede estar vacio",
+        patternMismatch:"El formato requerido es xxx-xxx-xx-xx"
+    },
+    direccion:{
+        valueMissing:"Este campo no puede estar vacio",
+        patternMismatch:"La direccion debe tener entre 10 y 40 caracteres."
+    },
+    ciudad:{
+        valueMissing:"Este campo no puede estar vacio",
+        patternMismatch:"La ciudad debe tener entre 10 y 40 caracteres."
+    },
+    estado:{
+        valueMissing:"Este campo no puede estar vacio",
+        patternMismatch:"El estado debe tener entre 10 y 40 caracteres."
+    },
+
+}
+
+
+const validadores ={
+    nacimiento: (input) => validarNacimiento(input), 
+}
+
+function mostrarMensajeDeError(tipoDeInput, input){
+    let mensaje ="";
+    tipoDeErrores.forEach((error) =>{
+        if(input.validity[error]){
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]); 
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+
+    return mensaje;
+}
+
+/*const inputNacimiento = document.querySelector("#birth");
+
+inputNacimiento.addEventListener("blur",(evento) =>{
+    validarNaciemiento(evento.target);
+
+});*/
+
+function validarNacimiento(input){
+    const fechaCliente = new Date (input.value);
+    let mensaje ="";
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje = "Debes tener al menos 18 aos de edad";
+    }
+     
+    input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate()
+    ); 
+    return diferenciaFechas <= fechaActual;
+
+}
